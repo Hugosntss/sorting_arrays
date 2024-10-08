fbbcfbcdd20c2dc1b9c04a05bd5c083cff02a26e
Hugo Santos <124203466+Hugosntss@users.noreply.github.com>
2024-10-07 22:45:24 -0700

    sorting array program

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "./utils/sort_array.cpp"
+using namespace std;
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    sortArray(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
