efed08c24f8e1c6499a0c86ba13304dbedb61449
Hugo Santos <124203466+Hugosntss@users.noreply.github.com>
2024-10-07 22:45:43 -0700

    utils added for sorting arrays

---
+++ utils/sort_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "sort_array.hpp"
+
+void sortArray(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
